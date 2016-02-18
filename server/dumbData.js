5b9afc511a12c4dc3cf945e8784a2ee16abd0a8e
dexterneo <jeremyfourna@gmail.com>
2016-02-18 15:35:34 +0100

    Add a second question in dumbdata

--- server/dumbData.js
+++ server/dumbData.js
@@ -133,6 +133,135 @@ Meteor.startup(function() {
 			questionType: 'standard',
 			displayType: 'text',
 			title: 'Qui suis je là maintenant ?'
+		}, {
+			questionId: Random.id(),
+			userId: user,
+			answered: false,
+			level: 0,
+			version: 1,
+			choices: [{
+				choiceId: Random.id(),
+				label: 'Seul',
+				universLinked: [{
+					universId: 'A',
+					matchingPower: 0.9
+				}, {
+					universId: 'B',
+					matchingPower: 0.8
+				}],
+				workshopLinked: [{
+					workshopId: 'C',
+					matchingPower: 0.7
+				}, {
+					workshopId: 'D',
+					matchingPower: 0.6
+				}]
+			}, {
+				choiceId: Random.id(),
+				label: 'Perdu',
+				universLinked: [{
+					universId: 'C',
+					matchingPower: 0.9
+				}, {
+					universId: 'D',
+					matchingPower: 0.8
+				}],
+				workshopLinked: [{
+					workshopId: 'A',
+					matchingPower: 0.7
+				}, {
+					workshopId: 'B',
+					matchingPower: 0.6
+				}]
+			}, {
+				choiceId: Random.id(),
+				label: 'Concentré sur un objectif',
+				universLinked: [{
+					universId: 'E',
+					matchingPower: 0.9
+				}, {
+					universId: 'F',
+					matchingPower: 0.8
+				}],
+				workshopLinked: [{
+					workshopId: 'G',
+					matchingPower: 0.7
+				}, {
+					workshopId: 'H',
+					matchingPower: 0.6
+				}]
+			}, {
+				choiceId: Random.id(),
+				label: "Envie d'agir",
+				universLinked: [{
+					universId: 'G',
+					matchingPower: 0.9
+				}, {
+					universId: 'H',
+					matchingPower: 0.8
+				}],
+				workshopLinked: [{
+					workshopId: 'E',
+					matchingPower: 0.7
+				}, {
+					workshopId: 'F',
+					matchingPower: 0.6
+				}]
+			}],
+			deprecated: false,
+			universLinked: [{
+				universId: 'G',
+				matchingPower: 0.9
+			}, {
+				universId: 'H',
+				matchingPower: 0.8
+			}, {
+				universId: 'A',
+				matchingPower: 0.9
+			}, {
+				universId: 'B',
+				matchingPower: 0.8
+			}, {
+				universId: 'C',
+				matchingPower: 0.9
+			}, {
+				universId: 'D',
+				matchingPower: 0.8
+			}, {
+				universId: 'E',
+				matchingPower: 0.9
+			}, {
+				universId: 'F',
+				matchingPower: 0.8
+			}],
+			workshopLinked: [{
+				workshopId: 'G',
+				matchingPower: 0.9
+			}, {
+				workshopId: 'H',
+				matchingPower: 0.8
+			}, {
+				workshopId: 'A',
+				matchingPower: 0.9
+			}, {
+				workshopId: 'B',
+				matchingPower: 0.8
+			}, {
+				workshopId: 'C',
+				matchingPower: 0.9
+			}, {
+				workshopId: 'D',
+				matchingPower: 0.8
+			}, {
+				workshopId: 'E',
+				matchingPower: 0.9
+			}, {
+				workshopId: 'F',
+				matchingPower: 0.8
+			}],
+			questionType: 'standard',
+			displayType: 'text',
+			title: 'Comment je me sens ?'
 		}];
 		for (var i = 0; i < questionList.length; i++) {
 			UserQuestions.insert(questionList[i], function(error, result) {
