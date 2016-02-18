e1cd04a19bd505806b51e1834a2f83bd3e5232ec
dexterneo <jeremyfourna@gmail.com>
2016-02-18 17:23:42 +0100

    Update and fix the display of the questions

--- client/templates/components/questions/questions.js
+++ client/templates/components/questions/questions.js
@@ -7,6 +7,9 @@ class Questions extends BlazeComponent {
 	onRendered() {
 		$('#radioBtn a').first().removeClass('notActive').addClass('active');
 		$('#radioBtn a').on('click', function() {
+			for (var i = 0; i < $('#radioBtn a').length; i++) {
+				console.log($('#radioBtn a').eq(i).data('title'));
+			}
 			var sel = $(this).data('title');
 			var tog = $(this).data('toggle');
 			$('#' + tog).prop('value', sel);
@@ -19,10 +22,54 @@ class Questions extends BlazeComponent {
 		return 'questions';
 	}
 
-	events() {
-		return super.events().concat({});
+	choiceIndex() {
+		if (this.currentData('@index') === 0) {
+			return 'active';
+		} else {
+			return 'notActive';
+		}
 	}
 
+	/*questionsAnswered() {
+		console.log(Meteor.call('questionAnsweredCount', Meteor.userId(), function(error, result) {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				return result;
+			}
+		}));
+		return Meteor.call('questionAnsweredCount', Meteor.userId(), function(error, result) {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				return result;
+			}
+		});
+	}*/
+
+	events() {
+		return super.events().concat({
+			'click #nextQuestion': this.saveAnswer,
+			'click .radioBtn': this.selectChoice
+		});
+	}
+
+	selectChoice(e) {
+		var sel = $(e.target).data('title');
+		var tog = $(e.target).data('toggle');
+		$('#' + tog).prop('value', sel);
+		$('a[data-toggle="' + tog + '"]').not('[data-title="' + sel + '"]').removeClass('active').addClass('notActive');
+		$('a[data-toggle="' + tog + '"][data-title="' + sel + '"]').removeClass('notActive').addClass('active');
+	}
+
+	saveAnswer() {
+		var choice = $('#fun').val();
+		Meteor.call('saveAnswer', this.currentData().questionId, choice, function(error, result) {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
+	}
 }
 
 Questions.register('Questions');
