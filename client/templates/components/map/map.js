b28723ae717ae5a75a43fb59f624d502ce46cc15
dexterneo <jeremyfourna@gmail.com>
2016-02-22 20:42:41 +0100

    Add a map component

---
+++ client/templates/components/map/map.js
@@ -0,0 +1,17 @@
+class Map extends BlazeComponent {
+	onRendered() {
+		L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
+		var map = L.map('map', {
+			doubleClickZoom: true
+		}).setView([48.866117, 2.313074], 15);
+		var marker = L.marker([48.866117, 2.313074]).addTo(map);
+
+		L.tileLayer.provider('OpenStreetMap.France').addTo(map);
+	}
+
+	template() {
+		return 'map';
+	}
+}
+
+Map.register('Map');
