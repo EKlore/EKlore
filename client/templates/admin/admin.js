c90d9358beb970147d45a1d976a6081c8af28c75
dexterneo <jeremyfourna@gmail.com>
2016-02-27 17:48:18 +0100

    Transfer part into separate templates

--- client/templates/admin/admin.js
+++ client/templates/admin/admin.js
@@ -1,21 +1,9 @@
 Template.admin.helpers({
-	isAdmin() {
+	isAdmin: function() {
 		if (Meteor.user().profile.admin) {
 			return true;
 		} else {
 			return false;
 		}
-	},
-	universesCount() {
-		return Universes.find({}).count();
-	},
-	universe() {
-		return Universes.find({});
-	},
-	workshopsCount() {
-		return Workshops.find({}).count();
-	},
-	workshop() {
-		return Workshops.find({});
-	},
+	}
 });
