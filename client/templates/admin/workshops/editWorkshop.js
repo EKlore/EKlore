39283869590f47654fa73326430da2842007ac55
dexterneo <jeremyfourna@gmail.com>
2016-02-28 14:12:43 +0100

    Add method and edition feature

--- client/templates/admin/workshops/editWorkshop.js
+++ client/templates/admin/workshops/editWorkshop.js
@@ -67,6 +67,38 @@ Template.editWorkshop.events({
 			}
 		}
 	},
+	'click #save': function(event) {
+		event.preventDefault();
+		var workshopData = {
+			workshopId: Router.current().params._id,
+			name: $('#workshopName').val(),
+			description: $('#workshopDescription').val(),
+			dateStart: moment($('#workshopDateStart').val()).toDate(),
+			dateEnd: moment($('#workshopEndDate').val()).toDate()
+		};
+		if (!workshopData.name) {
+			return throwError('Name must be filled');
+		}
+		if (!workshopData.description) {
+			return throwError('Label must be filled');
+		}
+		if (!workshopData.dateStart) {
+			return throwError('Date start must be filled');
+		}
+		if (!workshopData.dateEnd) {
+			return throwError('Date end must be filled');
+		}
+		if (workshopData.dateEnd < workshopData.dateStart) {
+			return throwError('Date start must be inferior to Date end');
+		}
+		Meteor.call('updateAWorkshop', workshopData, function(error, result) {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				return throwError('Update succesful !');
+			}
+		});
+	},
 	'submit .removeUniverseFromWorkshop': function(event) {
 		event.preventDefault();
 		var universeData = {
