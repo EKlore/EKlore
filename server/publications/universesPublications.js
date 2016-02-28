df95ce228015a03efabd25b9b0cf0f55667fda5b
dexterneo <jeremyfourna@gmail.com>
2016-02-28 13:46:31 +0100

    Add publications

--- server/publications/universesPublications.js
+++ server/publications/universesPublications.js
@@ -2,7 +2,7 @@ Meteor.publish('allUniverses', function() {
 	return Universes.find({});
 });
 
-Meteor.publish('aUniverse', function(universeId) {
+Meteor.publish('anUniverse', function(universeId) {
 	return Universes.find({
 		_id: universeId
 	});
--- server/publications/workshopsPublications.js
+++ server/publications/workshopsPublications.js
@@ -1,3 +1,9 @@
 Meteor.publish('allWorkshops', function() {
 	return Workshops.find({});
 });
+
+Meteor.publish('aWorkshop', function(workshopId) {
+	return Workshops.find({
+		_id: workshopId
+	});
+});
