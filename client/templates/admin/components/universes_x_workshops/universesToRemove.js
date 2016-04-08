c42e6842e66dfdf1c484bc92a081e61db81ed001
dexterneo <jeremyfourna@gmail.com>
2016-04-08 15:38:38 +0200

    Move templates into folders

---
+++ client/templates/admin/components/universes_x_workshops/universesToRemove.js
@@ -0,0 +1,26 @@
+Template.universesToRemove.helpers({
+	universeData() {
+		return Universes.findOne(this.universeId);
+	}
+});
+
+Template.universesToRemove.events({
+	'submit .removeUniverseFromWorkshop': function(event) {
+		event.preventDefault();
+		var universeData = {
+			workshopId: Router.current().params._id,
+			universeId: this.universeId,
+		};
+		Meteor.call('removeUniverseFromWorkshop', universeData, (error, result) => {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				Meteor.call('removeWorkshopFromUniverse', universeData, (error, result) => {
+					if (error) {
+						return throwError(error.message);
+					}
+				});
+			}
+		});
+	}
+});
---
+++ client/templates/admin/components/universes_x_workshops/workshopsToAdd.js
@@ -0,0 +1,31 @@
+Template.workshopsToAdd.events({
+	'submit .addWorkshopToUniverse': function(event) {
+		event.preventDefault();
+		var workshopData = {
+			universeId: Router.current().params._id,
+			workshopId: this._id,
+			matchingPower: Number($(event.target).find('.matchingPower').val())
+		};
+		if (!workshopData.matchingPower) {
+			return throwError('The matching power of the workshop must be filled');
+		} else {
+			if (workshopData.matchingPower <= 0) {
+				return throwError('The matching power of the workshop must be superior to 0');
+			} else if (workshopData.matchingPower > 1) {
+				return throwError('The matching power of the workshop must be inferior to 1');
+			} else {
+				Meteor.call('addWorkshopToUniverse', workshopData, (error, result) => {
+					if (error) {
+						return throwError(error.message);
+					} else {
+						Meteor.call('addUniverseToWorkshop', workshopData, (error, result) => {
+							if (error) {
+								return throwError(error.message);
+							}
+						});
+					}
+				});
+			}
+		}
+	}
+});
