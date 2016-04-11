029cecd6ef71cc907f90b77df82490ec02e608b8
dexterneo <jeremyfourna@gmail.com>
2016-04-11 16:44:48 +0200

    Add publications and optimize old one

--- server/publications/ekloreQuestionsPublications.js
+++ server/publications/ekloreQuestionsPublications.js
@@ -1,3 +1,13 @@
 Meteor.publish('allEkloreQuestions', () => {
-	return EkloreQuestions.find({});
+	return EkloreQuestions.find({
+		deprecated: false
+	}, {
+		fields: {
+			title: 1,
+		}
+	});
+});
+
+Meteor.publish('anEkloreQuestion', (ekloreQuestionId) => {
+	return EkloreQuestions.find({ _id: ekloreQuestionId });
 });
--- server/publications/questionsGroupsPublications.js
+++ server/publications/questionsGroupsPublications.js
@@ -1,9 +1,22 @@
 Meteor.publish('allQuestionsGroups', () => {
-	return QuestionsGroups.find({});
+	return QuestionsGroups.find({}, {
+		fields: {
+			title: 1,
+			label: 1,
+			deprecated: 1
+		}
+	});
 });
 
 Meteor.publish('aQuestionsGroup', (questionGroupId) => {
 	return QuestionsGroups.find({
 		_id: questionGroupId
+	});
+});
+
+Meteor.publish('aQuestionsGroupForAnEkloreQuestion', (ekloreQuestionId) => {
+	const ekloreQuestion = EkloreQuestions.findOne({ _id: ekloreQuestionId });
+	return QuestionsGroups.find({
+		_id: ekloreQuestion.questionGroupId
 	});
 });
