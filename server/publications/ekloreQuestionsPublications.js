5e2338260f99e5879237c02dd38ae24f4d8027c1
dexterneo <jeremyfourna@gmail.com>
2016-04-18 18:00:21 +0200

    Add publication

--- server/publications/ekloreQuestionsPublications.js
+++ server/publications/ekloreQuestionsPublications.js
@@ -3,11 +3,21 @@ Meteor.publish('allEkloreQuestions', () => {
 		fields: {
 			title: 1,
 			version: 1,
-			displayType: 1
+			displayType: 1,
+			questionGroupId: 1
 		}
 	});
 });
 
 Meteor.publish('anEkloreQuestion', (ekloreQuestionId) => {
 	return EkloreQuestions.find({ _id: ekloreQuestionId });
+});
+
+Meteor.publish('ekloreQuestionsLinkedToQuestionsGroup', (questionGroupId) => {
+	return EkloreQuestions.find({ questionGroupId }, {
+		fields: {
+			title: 1,
+			questionGroupId: 1
+		}
+	});
 });
