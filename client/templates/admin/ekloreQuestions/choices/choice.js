ce3b56537ced66ee42faa149df0e4689cfd6b194
dexterneo <jeremyfourna@gmail.com>
2016-04-26 14:55:44 +0200

    Add workshops display and adding feature to choice

--- client/templates/admin/ekloreQuestions/choices/choice.js
+++ client/templates/admin/ekloreQuestions/choices/choice.js
@@ -29,5 +29,58 @@ Template.choice.helpers({
 				}
 			});
 		}
+	},
+	workshops() {
+		if (!Template.parentData(1).workshopsLinked || Template.parentData(1).workshopsLinked.length === 0) {
+			return false;
+		} else if (!this.workshopsLinked || this.workshopsLinked.length === 0) {
+			let qStart = { _id: { $in: [] } };
+			Template.parentData(1).workshopsLinked.map((cur, index, array) => {
+				return qStart._id['$in'].push(cur.workshopId);
+			});
+			return Workshops.find(qStart, {
+				fields: {
+					name: 1
+				}
+			});
+		} else {
+			let qStart = { $and: [] };
+			let qIn = { _id: { $in: [] } };
+			let qNin = { _id: { $nin: [] } };
+			Template.parentData(1).workshopsLinked.map((cur, index, array) => {
+				return qIn._id['$in'].push(cur.workshopId);
+			});
+			this.workshopsLinked.map((cur, index, array) => {
+				return qNin._id['$nin'].push(cur.workshopId);
+			});
+			qStart['$and'].push(qIn, qNin);
+			return Workshops.find(qStart, {
+				fields: {
+					name: 1
+				}
+			});
+		}
+	}
+});
+
+Template.choice.events({
+	'submit .saveLabel': function(event) {
+		event.preventDefault();
+		const data = {
+			ekloreQuestionId: Router.current().params._id,
+			choiceId: this.choiceId,
+			label: $(event.target).find('#' + this.choiceId).val()
+		};
+		console.log(data, event.target);
+		if (!data.label) {
+			return throwError('Label must be filled');
+		}
+		/*Meteor.call('updateAChoice', data, (error, result) => {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				return throwError('Update succesful !');
+			}
+		});*/
 	}
 });
