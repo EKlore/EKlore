14617ca106f0a948d3c6fd09bcebaa7d60930e7a
dexterneo <jeremyfourna@gmail.com>
2016-05-02 14:09:18 +0200

    Add feature to answer question

---
+++ client/templates/front/answerQuestions/answerQuestions.js
@@ -0,0 +1,31 @@
+Template.answerQuestions.helpers({
+	questionData() {
+		return UserQuestions.findOne({
+			userId: Meteor.userId(),
+			deprecated: false,
+			answered: false
+		}, {
+			sort: {
+				level: 1
+			}
+		});
+	}
+});
+
+Template.answerQuestions.events({
+	'click #validateChoice': function(event) {
+		event.preventDefault();
+		const data = {
+			userQuestionId: this._id,
+			choiceSelected: $('input[name="choicesForQuestion"]:checked').val()
+		};
+		if (!data.choiceSelected) {
+			return throwError('You must choose an answer !');
+		}
+		Meteor.call('answerQuestion', data, (error, result) => {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
+	}
+});
