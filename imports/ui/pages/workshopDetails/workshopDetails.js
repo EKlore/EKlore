e333ae1de8749001b11a883d18157126dbb9c330
dexterneo <jeremyfourna@gmail.com>
2016-06-07 13:44:03 +0200

    Add page to see workshop detail

---
+++ imports/ui/pages/workshopDetails/workshopDetails.js
@@ -0,0 +1,66 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { lodash } from 'meteor/stevezhu:lodash';
+
+import { Workshops } from '../../../api/workshops/schema.js';
+
+import './workshopDetails.jade';
+
+Template.workshopDetails.onCreated(function() {
+	this.autorun(() => {
+		this.subscribe('aWorkshop', Router.current().params._id);
+	});
+});
+
+Template.workshopDetails.helpers({
+	workshop() {
+		return Workshops.findOne({ _id: Router.current().params._id });
+	},
+	dateStart() {
+		return moment(this.dateStart).format('H:mm');
+	},
+	dateEnd() {
+		return moment(this.dateEnd).format('H:mm');
+	},
+	isUserAlreadyIn() {
+		if (lodash.findIndex(this.peopleToGo, ['userId', Meteor.userId()]) !== -1) {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	isUserAlreadyIn() {
+		if (lodash.findIndex(this.peopleToGo, ['userId', Meteor.userId()]) !== -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+});
+
+Template.workshopDetails.events({
+	'click .goToWorkshop': function(event) {
+		event.preventDefault();
+		const data = {
+			userId: Meteor.userId(),
+			workshopId: this._id
+		};
+		Meteor.call('addUserToWorkshop', data, (error, result) => {
+			if (error) {
+				return Bert.alert(error.message, 'danger', 'growl-top-right');
+			}
+		});
+	},
+	'click .removeFromWorkshop': function(event) {
+		event.preventDefault();
+		const data = {
+			userId: Meteor.userId(),
+			workshopId: this._id
+		};
+		Meteor.call('removeUserFromWorkshop', data, (error, result) => {
+			if (error) {
+				return Bert.alert(error.message, 'danger', 'growl-top-right');
+			}
+		});
+	}
+});
