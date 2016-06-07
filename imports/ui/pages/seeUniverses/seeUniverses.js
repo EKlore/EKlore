0ee9c711ccc0b6adef9823da41dd85ac38451e79
dexterneo <jeremyfourna@gmail.com>
2016-06-07 13:43:45 +0200

    Add page to see all universes

---
+++ imports/ui/pages/seeUniverses/seeUniverses.js
@@ -0,0 +1,48 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import { Universes } from '../../../api/universes/schema.js';
+import { Workshops } from '../../../api/workshops/schema.js';
+
+import './seeUniverses.jade';
+
+Template.seeUniverses.onCreated(function() {
+	this.autorun(() => {
+		this.subscribe('allUniverses');
+		this.subscribe('allWorkshopsToSee');
+	});
+});
+
+Template.seeUniverses.helpers({
+	universes() {
+		return Universes.find({}, {
+			sort: {
+				name: 1
+			},
+			fields: {
+				name: 1,
+				label: 1,
+				workshopsLinked: 1
+			}
+		})
+	},
+	workshops() {
+		let query = [];
+		this.workshopsLinked.map((cur, index, array) => {
+			query.push(cur.workshopId);
+		});
+		return Workshops.find({
+			_id: {
+				$in: query
+			}
+		}, {
+			sort: {
+				name: 1
+			},
+			fields: {
+				name: 1,
+				description: 1
+			}
+		});
+	}
+});
