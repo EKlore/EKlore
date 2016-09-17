592e27f7650680d08c1bdfdb1dad328851fe2c3c
dexterneo <jeremyfourna@gmail.com>
2016-09-17 22:06:11 +0200

    Add publications

--- imports/api/volunteers/server/publications.js
+++ imports/api/volunteers/server/publications.js
@@ -6,6 +6,38 @@ Meteor.publish('allVolunteers', () => {
 	return Volunteers.find({});
 });
 
+Meteor.publish('topVolunteers', () => {
+	return Volunteers.find({
+		level: {
+			$lt: 10
+		}
+	}, {
+		sort: {
+			level: 1
+		}
+	});
+});
+
+Meteor.publish('midVolunteers', () => {
+	return Volunteers.find({
+		level: 10
+	}, {
+		sort: {
+			lastName: 1
+		}
+	});
+});
+
+Meteor.publish('lowVolunteers', () => {
+	return Volunteers.find({
+		level: 99
+	}, {
+		sort: {
+			lastName: 1
+		}
+	});
+});
+
 Meteor.publish('aVolunteer', (volunteerId) => {
 	return Volunteers.find({ _id: volunteerId });
 });
