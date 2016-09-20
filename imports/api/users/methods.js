ee2a89d0a1173ed2cdf1181cc2e4cc294504888c
dexterneo <jeremyfourna@gmail.com>
2016-09-20 13:03:39 +0200

    WIP first method to Hello Talent API

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { HTTP } from 'meteor/http';
 
 Meteor.methods({
 	addQuestionsGroupIntoUser(data) {
@@ -22,5 +23,21 @@ Meteor.methods({
 				'profile.wantsToBeVolunteer': true
 			}
 		});
+	},
+	HTGetToken() {
+		const options = {
+			content: '',
+			data: {},
+			query: '',
+			params: {
+				grant_type: 'client_credentials',
+				client_id: '',
+				client_secret: ''
+			},
+			auth: 'username:password',
+			header: {},
+			timeout: 3000
+		};
+		HTTP.post('https://app.hellotalent.com/api/token')
 	}
 });
