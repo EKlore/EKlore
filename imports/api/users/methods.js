19b8c02d448806ffdd2a74590f7a3ada7e909cc4
jeremyfourna <jeremyfourna@gmail.com>
2017-01-10 09:54:03 +0100

    Add comments and refactor method

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -5,19 +5,24 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { UserQuestions } from '../userQuestions/schema.js';
 
 Meteor.methods({
+	/*
+	This method will add for a user all the questions linked to
+	a questions group
+	*/
 	addQuestionsGroupIntoUser(data) {
+		// Check method params
 		let methodSchema = new SimpleSchema({
 			userId: { type: String },
 			questionsGroupId: { type: String }
 		});
 		check(data, methodSchema);
-		let nbQuestions = UserQuestions.find({
-			userId: data.userId
-		}, {
+		// If OK the code continue
+		let nbQuestions = UserQuestions.find({ userId: data.userId }, {
 			fields: {
 				_id: 1
 			}
 		}).count();
+
 		return Meteor.users.update({ _id: data.userId }, {
 			$addToSet: {
 				'profile.questionsGroups': data.questionsGroupId
@@ -28,39 +33,38 @@ Meteor.methods({
 		});
 	},
 	becomeVolunteer(userId) {
+		// Check method params
 		check(userId, String);
+		// If OK the code continue
 		return Meteor.users.update({ _id: userId }, {
 			$set: {
 				'profile.wantsToBeVolunteer': true
 			}
 		});
 	},
+	/*
+	This method will fix the number of questions for every user
+	*/
 	fixNbQuestions() {
 		let users = Meteor.users.find({}, {
 			fields: {
 				_id: 1
 			}
 		});
-		users.map((cur) => {
-			let nbAnswered = UserQuestions.find({
-				userId: cur._id,
-				answered: true
-			}, {
+		return users.map((cur) => {
+			let nbQuestions = UserQuestions.find({ userId: cur._id }, {
 				fields: {
-					_id: 1
+					_id: 1,
+					answered: 1
 				}
-			}).count();
-			let nbQuestions = UserQuestions.find({
-				userId: cur._id
-			}, {
-				fields: {
-					_id: 1
-				}
-			}).count();
+			}).fetch();
+			let nbAnswered = nbQuestions.filter((cur) => {
+				return cur.nbAnswered = true;
+			});
 			return Meteor.users.update({ _id: cur._id }, {
 				$set: {
-					'profile.nbAnswered': nbAnswered,
-					'profile.nbQuestions': nbQuestions
+					'profile.nbAnswered': nbAnswered.length,
+					'profile.nbQuestions': nbQuestions.length
 				}
 			});
 		});
